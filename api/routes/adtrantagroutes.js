a52069f421f7d97c6bb0cfc67a9cf618685f7b72
bryanpham85 <duchungpham12d2@gmail.com>
2017-08-30 14:14:45 +0700

    update content type for nonscript

--- api/routes/adtrantagroutes.js
+++ api/routes/adtrantagroutes.js
@@ -11,20 +11,20 @@ module.exports = function(app){
 
 //define configuration for caching
 
-var cache = (duration) =>{
+var cache = (duration, contenttype) =>{
     return (req, res, next) =>{
         var date = new Date(Date.now()).toDateString();
         var time = new Date(Date.now()).toTimeString();
         let key = '__adtranonetag__' + req.originalUrl || req.url
         let cachedBody = mcache.get(key)
         if (cachedBody) {
-            res.setHeader("content-type", "text/javascript");
+            res.setHeader("content-type", contenttype);
             res.send(cachedBody);
             console.log("get from Cache for " + key + " at " + date + " " + time);
             return;
         } else {
             console.log("get from DB for key + " + key + " at " + date + " " + time);
-            res.header("content-type", "text/javascript");
+            res.header("content-type", contenttype);
             res.sendResponse = res.send
             res.send = (body) => {
                 mcache.put(key, body, duration * 1000);
@@ -36,10 +36,10 @@ var cache = (duration) =>{
 }
 
     app.route('/container.js')
-        .get(cache(config.cacheduration),adtranTagController.getTagListByContainerId);
+        .get(cache(config.cacheduration, "text/javasript"),adtranTagController.getTagListByContainerId);
 
     app.route('/nscontainer.html')
-        .get(cache(config.cacheduration),adtranTagController.getNSTagListByContainerId);
+        .get(cache(config.cacheduration, "text/html"),adtranTagController.getNSTagListByContainerId);
 
     app.route('/containers')
         .get(adtranTagController.getContainerList)
