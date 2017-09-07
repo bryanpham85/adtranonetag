66d94e8b324267ab94ac05688bfc436e2dd3deb1
bryanpham85 <duchungpham12d2@gmail.com>
2017-09-07 13:15:44 +0700

    add log4js

--- api/routes/adtrantagroutes.js
+++ api/routes/adtrantagroutes.js
@@ -7,16 +7,37 @@
 module.exports = function(app){
     var mcache = require('memory-cache'),
     adtranTagController = require('../controllers/adtrantagcontroller'),
-    config = require('../../config').get(process.env.NODE_ENV);
-
+    config = require('../../config').get(process.env.NODE_ENV),
+    log4js = require('log4js');
+//configure log4js
+log4js.configure({
+   appenders:{
+       tagReqLogs: {type: 'dateFile', filename:'log/requestlog.log', pattern:'-yyyy-MM-dd', 'alwaysIncludePattern':true},
+       console: {type: 'console'}
+   },
+    categories: {
+       requestLog: {appenders:['tagReqLogs'], level:'info'},
+        default: {appenders: ['console'], level:'trace'}
+    }
+});
 //define configuration for caching
+const logger = log4js.getLogger('requestLog');
 
 var cache = (duration, contenttype) =>{
     return (req, res, next) =>{
         var date = new Date(Date.now()).toDateString();
         var time = new Date(Date.now()).toTimeString();
-        let key = '__adtranonetag__' + req.originalUrl || req.url
-        let cachedBody = mcache.get(key)
+        let key = '__adtranonetag__' + req.originalUrl || req.url;
+        let cachedBody = mcache.get(key);
+        //Proceed log to file the info of request.
+        var agent = req.get('User-Agent');
+        var ip = (req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress).split(",")[0];
+
+        logger.info(key + " from " + agent + " with IP " + ip);
+
         if (cachedBody) {
             res.setHeader("content-type", contenttype);
             res.send(cachedBody);
